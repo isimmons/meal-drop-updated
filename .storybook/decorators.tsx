e03defdef5a39e907bcf8440096f2affb0ae62d2
isimmons <isimmons33@gmail.com>
2023-06-02 04:04:45 -0400

    withDesign import no longer needed as a decarator in v7

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Decorator } from "@storybook/react";
-import { withDesign } from "storybook-addon-designs";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../src/styles/GlobalStyle";
 import { lightTheme, darkTheme } from "../src/styles/theme";
@@ -18,4 +17,4 @@ const withTheme: Decorator = (StoryFn, context) => {
   );
 };
 
-export const globalDecorators = [withTheme, withDesign];
+export const globalDecorators = [withTheme];
